1e3b6df13c7e39e3f405eef45b8f3e6dc03c4127
Sepehr Movasat <Sepy7780@gmail.com>
2023-10-07 22:14:29 +0330

    Add simple mode

--- Heart_AI.py
+++ Heart_AI.py
@@ -28,7 +28,10 @@ class ML_Model():
             print(f"{UI.magenta_text('Gathering Data:')} {UI.red_text('[')}                     {UI.red_text(']')} {UI.cyan_text('0%')}")
             UI.Input_Style()
             age = int(input("Input your age: "))
-            bar = UI.ProgressBar(11,"Gathering Data")
+            if simple:
+                bar = UI.ProgressBar(5,"Gathering Data")
+            else:
+                bar = UI.ProgressBar(11,"Gathering Data")
             match age:
                 case _ if age >= 120:
                     raise RealityWarpErrors("Hold up! Age above 120? Unless you're a time traveler, let's keep it reasonable.")
@@ -61,45 +64,49 @@ Input your chest pain type {UI.red_text('(JUST THE NUMBER)')}: """))
             user_input.append(cp)
             bar.advance_progress()
 
-            UI.Input_Style()
-            trtbps = int(input("Input your resting blood pressure: "))
-            match trtbps:
-                case _ if trtbps < 30:
-                    raise RealityWarpErrors("Your blood pressure is so low that it's in stealth mode. Are you sure you're not a ghost?")
-                case _ if trtbps > 220:
-                    raise RealityWarpErrors("Your blood pressure is so high, even our computer is feeling the pressure and starting to glitch.")
-            user_input.append(trtbps)
-            bar.advance_progress()
+            if not simple:
+                UI.Input_Style()
+                trtbps = int(input("Input your resting blood pressure: "))
+                match trtbps:
+                    case _ if trtbps < 30:
+                        raise RealityWarpErrors("Your blood pressure is so low that it's in stealth mode. Are you sure you're not a ghost?")
+                    case _ if trtbps > 220:
+                        raise RealityWarpErrors("Your blood pressure is so high, even our computer is feeling the pressure and starting to glitch.")
+                user_input.append(trtbps)
+                bar.advance_progress()
 
-            UI.Input_Style()
-            chol = int(input("Input your blood's cholesterol level: "))
-            match chol:
-                case _ if chol < 80:
-                    raise RealityWarpErrors("Eating healthy's good, but not this healthy. Cholesterol can't be less than 80.")
-                case _ if chol > 500:
-                    raise RealityWarpErrors("Your cholesterol level is so elevated that even our algorithms are considering a career change to become heart doctors!")
-            user_input.append(chol)
-            bar.advance_progress()
+            if not simple:
+                UI.Input_Style()
+                chol = int(input("Input your blood's cholesterol level: "))
+                match chol:
+                    case _ if chol < 80:
+                        raise RealityWarpErrors("Eating healthy's good, but not this healthy. Cholesterol can't be less than 80.")
+                    case _ if chol > 500:
+                        raise RealityWarpErrors("Your cholesterol level is so elevated that even our algorithms are considering a career change to become heart doctors!")
+                user_input.append(chol)
+                bar.advance_progress()
 
-            UI.Input_Style()
-            fbs = int(input(f"Is your fasting blood sugar more than 120 mg/dl [{UI.red_text('1')} for {UI.red_text('Yes')}, {UI.green_text('0')} for {UI.green_text('no')}]?"))
-            match fbs:
-                case _ if not (fbs == 0 or fbs == 1):
-                    raise RealityWarpErrors("Hold the sugar! The fasting blood sugar should be either more than 120 mg/dl (1) or less than that (0).")
-            user_input.append(fbs)
-            bar.advance_progress()
+            if not simple:
+                UI.Input_Style()
+                fbs = int(input(f"Is your fasting blood sugar more than 120 mg/dl [{UI.red_text('1')} for {UI.red_text('Yes')}, {UI.green_text('0')} for {UI.green_text('no')}]? "))
+                match fbs:
+                    case _ if not (fbs == 0 or fbs == 1):
+                        raise RealityWarpErrors("Hold the sugar! The fasting blood sugar should be either more than 120 mg/dl (1) or less than that (0).")
+                user_input.append(fbs)
+                bar.advance_progress()
 
-            UI.Input_Style()
-            restecg = int(input(f"""
+            if not simple:
+                UI.Input_Style()
+                restecg = int(input(f"""
 {UI.magenta_text('0-')} normal
 {UI.magenta_text('1-')} having ST-T wave abnormality
 {UI.magenta_text('2-')} probable left ventricular hypertrophy
 Input your resting electrocardiographic results{UI.red_text('(JUST THE NUMBER)')}: """))
-            match restecg:
-                case _ if not (restecg == 0 or restecg == 1 or restecg == 2):
-                    raise TypeError("Heart's beat is 0, 1, or 2. Pick one of those, not your own rhythm.")
-            user_input.append(restecg)
-            bar.advance_progress()
+                match restecg:
+                    case _ if not (restecg == 0 or restecg == 1 or restecg == 2):
+                        raise TypeError("Heart's beat is 0, 1, or 2. Pick one of those, not your own rhythm.")
+                user_input.append(restecg)
+                bar.advance_progress()
 
             UI.Input_Style()
             thalachh = int(input("Input the maximum heart rate you achived (during exercise): "))
@@ -119,22 +126,24 @@ Input your resting electrocardiographic results{UI.red_text('(JUST THE NUMBER)')
             user_input.append(exng)
             bar.advance_progress()
 
-            UI.Input_Style()
-            slp = int(input(f"Input your slope of the peak exercise ST segment ({UI.magenta_text('1:')} {UI.green_text('upsloping')}, {UI.magenta_text('2:')} {UI.yellow_text('flat')}, {UI.magenta_text('3:')} {UI.red_text('downsloping')}): "))
-            match slp:
-                case _ if not (slp == 1 or slp == 2 or slp == 3):
-                    raise TypeError("Hang on a sec! The slope of the peak exercise ST segment should be 1, 2 or 3.")
-            user_input.append(slp)
-            bar.advance_progress()
+            if not simple:
+                UI.Input_Style()
+                slp = int(input(f"Input your slope of the peak exercise ST segment ({UI.magenta_text('1:')} {UI.green_text('upsloping')}, {UI.magenta_text('2:')} {UI.yellow_text('flat')}, {UI.magenta_text('3:')} {UI.red_text('downsloping')}): "))
+                match slp:
+                    case _ if not (slp == 1 or slp == 2 or slp == 3):
+                        raise TypeError("Hang on a sec! The slope of the peak exercise ST segment should be 1, 2 or 3.")
+                user_input.append(slp)
+                bar.advance_progress()
 
-            UI.Input_Style()
-            caa = int(input(f"Input the number of your major vessels {UI.cyan_text('(0-3)')} (colored by fluoroscopy): "))
-            match caa:
-                case _ if not caa >= 0:
-                    raise TypeError("Whoa there! Your major vessels are like secret agents. They're so covert that even CIA is asking for tips.")
-                case _ if not  caa <= 3:
-                    raise TypeError("Major vessels overload! Your heart has so many highways that even Google Maps is getting confused.")
-            user_input.append(caa)
+            if not simple:
+                UI.Input_Style()
+                caa = int(input(f"Input the number of your major vessels {UI.cyan_text('(0-3)')} (colored by fluoroscopy): "))
+                match caa:
+                    case _ if not caa >= 0:
+                        raise TypeError("Whoa there! Your major vessels are like secret agents. They're so covert that even CIA is asking for tips.")
+                    case _ if not  caa <= 3:
+                        raise TypeError("Major vessels overload! Your heart has so many highways that even Google Maps is getting confused.")
+                user_input.append(caa)
             bar.finish()
 
             user_input = np.array(user_input)
@@ -169,15 +178,19 @@ Input your resting electrocardiographic results{UI.red_text('(JUST THE NUMBER)')
                 UI.main_menu()
             UI.main_menu()
 
-    def Train_model():
+    def Train_model(simple=False):
         global model
         """Trains the model"""
         # The data frame from the csv file
         df = pd.read_csv("Data/heart.csv")
 
         # Modeling here - As data cleaning just lowers the accuracy we don't do it
-        features = ['age', 'sex', 'cp', 'trtbps', 'chol', 'fbs', 'restecg', 'thalachh', 'exng', 'slp', 'caa']
-        target = 'output'
+        if not simple:
+            features = ['age', 'sex', 'cp', 'trtbps', 'chol', 'fbs', 'restecg', 'thalachh', 'exng', 'slp', 'caa']
+            target = 'output'
+        else:
+            features = ['age', 'sex', 'cp', 'thalachh', 'exng']
+            target = 'output'
 
         # Splitting data into features and target
         X = df[features]
@@ -276,6 +289,7 @@ class UI:
 
     # And here it is, the main menu
     def main_menu(key=None):
+        global simple
         UI.cls()
         print(UI.red_text(f"""    ,⌂▒▒▒▒µ,    ¿φ@@@@@µ
   ▒▒▒▒▒▒▒▒▒▒▒N╥Ñ▒ÑÑÑ╫╫╫╫╫@¿
@@ -316,7 +330,34 @@ class UI:
 
         match inp:
             case 1:
-                ML_Model.Train_model()
+                UI.Input_Style()
+                try:
+                    UI.cls()
+                    print(UI.green_text("<-----------Mode Selection----------->"))
+                    inp2 = int(input(f"""{UI.cyan_text('Modes')}:
+{UI.magenta_text('1- Simple')} - 70% accurate, doesn't need complicated information
+{UI.magenta_text('2- Default')} - 85% accurate, needs complicated information
+Select the mode{UI.red_text("(Just enter the number)")}: """))
+                except ValueError as e:
+                    UI.Info_Style()
+                    print("Please do not enter anything more than numbers")
+                    UI.Error_Style()
+                    print(f'Error details: {e}')
+                    print(UI.magenta_text("Press enter to go back to the main menu..."))
+                    try:
+                        x = input("")
+                    except Exception:
+                        UI.main_menu()
+                    UI.main_menu()
+                match inp2:
+                    case 1:
+                        simple = True
+                    case 2:
+                        simple = False
+                    case _:
+                        UI.main_menu()
+
+                ML_Model.Train_model(simple=simple)
                 data = ML_Model.gather_data()
                 result = ML_Model.predict(data)
                 UI.cls()
@@ -325,7 +366,10 @@ class UI:
                     UI.Error_Style()
                     print(UI.red_text("You are likley to get a heart attack!"))
                     UI.Info_Style()
-                    print(f"{UI.red_text('NOTE:')} Our program is in {UI.cyan_text('beta stage')} and is about {UI.green_text('85% accurate')}")
+                    if not simple:
+                        print(f"{UI.red_text('NOTE:')} Our program is in {UI.cyan_text('beta stage')} and is about {UI.green_text('85% accurate')}")
+                    else:
+                        print(f"{UI.red_text('NOTE:')} Your are using the {UI.cyan_text('simple mode')} which is about {UI.green_text('70% accurate')}")
                     print(UI.yellow_text("BTW we recommend you to see a doctor"))
                     print(UI.magenta_text("Press enter to go back to the main menu..."))
                     try:  
@@ -337,8 +381,10 @@ class UI:
                 if not result:
                     UI.Great_Style()
                     print(UI.green_text("You are not likley to get a heart attack!"))
-                    UI.Info_Style()
-                    print(f"{UI.red_text('NOTE:')} Our program is in {UI.cyan_text('beta stage')} and is about {UI.green_text('85% accurate')}")
+                    if not simple:
+                        print(f"{UI.red_text('NOTE:')} Our program is in {UI.cyan_text('beta stage')} and is about {UI.green_text('85% accurate')}")
+                    else:
+                        print(f"{UI.red_text('NOTE:')} Your are using the {UI.cyan_text('simple mode')} which is about {UI.green_text('70% accurate')}")
                     print(UI.yellow_text("If you want to make sure you can see a doctor"))
                     print(UI.magenta_text("Press enter to go back to the main menu..."))
                     try:                 
